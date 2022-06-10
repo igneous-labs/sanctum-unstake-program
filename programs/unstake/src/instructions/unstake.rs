a08fc822f0bba31a63b71f4f88a36512a81163eb
Jesse Y. Cho <jesse@socean.fi>
2022-06-10 18:23:38 +0900

    add more boilerplate code for unstake

--- programs/unstake/src/instructions/unstake.rs
+++ programs/unstake/src/instructions/unstake.rs
@@ -1,16 +1,33 @@
 use anchor_lang::prelude::*;
 
+use crate::state::Pool;
+
 #[derive(Accounts)]
 pub struct Unstake<'info> {
     pub unstaker: Signer<'info>,
+    pub pool: Account<'info, Pool>,
+
+    #[account(
+        mut,
+        // TODO: constraint -> must be owned by the unstaker
+        // TODO: constraint -> must not be locked
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
     pub system_program: Program<'info, System>,
 }
 
+// This is just a placeholder
+// TODO: resolve this by either
+//  - research how to add account struct from a non-anchor program
+//  - check and follow the stake-pool program (where they marked the ported code as "copid from stake program")
+#[account]
+pub struct StakeAccount;
+
 impl<'info> Unstake<'info> {
-    const SIMPLE_FLAT_FEE: u64 = 1_000_000_000;
+    const _SIMPLE_FLAT_FEE: u64 = 1_000_000_000;
 
     #[inline(always)]
-    pub fn run(ctx: Context<Self>) -> Result<()> {
+    pub fn run(_ctx: Context<Self>) -> Result<()> {
         Ok(())
     }
 }
