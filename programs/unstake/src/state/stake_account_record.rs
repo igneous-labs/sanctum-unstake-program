de890c2bfd816e0e198f0b060e95462df9fc3254
billythedummy <dhy1996@live.com.sg>
2022-06-12 19:38:11 +0800

    doc

--- programs/unstake/src/state/stake_account_record.rs
+++ programs/unstake/src/state/stake_account_record.rs
@@ -5,6 +5,6 @@ pub struct StakeAccountRecord {
     /// The stake account's lamports in the associated stake account
     /// at time of Unstake.
     /// Note: this is the account's total lamports not staked lamports
-    /// Solana enforces this to be rent exempt balance + 1 lamport
+    /// Solana enforces this to be at least rent exempt balance + 1 lamport
     pub lamports_at_creation: u64,
 }
