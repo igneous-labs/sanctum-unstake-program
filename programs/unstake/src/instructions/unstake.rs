2bcffbdd431e3c65072e12bd488e7d0288ccdd5a
Jesse Y. Cho <jesse@socean.fi>
2022-06-13 19:29:18 +0900

    wip: try using StakeAccountRecord in unstake ix

--- programs/unstake/src/instructions/unstake.rs
+++ programs/unstake/src/instructions/unstake.rs
@@ -1,33 +1,56 @@
 use anchor_lang::prelude::*;
+use anchor_spl::stake::StakeAccount;
 
-use crate::state::Pool;
+use crate::anchor_len::AnchorLen;
+use crate::state::{Pool, StakeAccountRecord};
 
 #[derive(Accounts)]
 pub struct Unstake<'info> {
+    ///
+    #[account(mut)]
     pub unstaker: Signer<'info>,
+
+    ///
     pub pool: Account<'info, Pool>,
 
+    ///
     #[account(
         mut,
         // TODO: constraint -> must be owned by the unstaker
-        // TODO: constraint -> must not be locked
+        // TODO: constraint -> must not be locked (Deligated or Initialized)
     )]
     pub stake_account: Account<'info, StakeAccount>,
+
+    /// (PDA)
+    #[account(
+        init,
+        payer = unstaker,
+        space = StakeAccountRecord::LEN,
+    )]
+    pub stake_account_record: Account<'info, StakeAccountRecord>,
+
+    /// Solana native wallet pubkey to receive the unstaked amount
+    /// CHECK: payment destination that can accept sol transfer
+    pub destination: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
 }
-
-// This is just a placeholder
-// TODO: resolve this by either
-//  - research how to add account struct from a non-anchor program
-//  - check and follow the stake-pool program (where they marked the ported code as "copid from stake program")
-#[account]
-pub struct StakeAccount;
 
 impl<'info> Unstake<'info> {
     const _SIMPLE_FLAT_FEE: u64 = 1_000_000_000;
 
     #[inline(always)]
-    pub fn run(_ctx: Context<Self>) -> Result<()> {
+    pub fn run(ctx: Context<Self>) -> Result<()> {
+        // access the given stake account data
+        let _stake_account = &mut ctx.accounts.stake_account;
+
+        // populate the stake_account_record
+        //
         Ok(())
+    }
+
+    // TODO: predicate that validates given the state StakeAccount
+    pub fn validate_stake_account_state(&self) -> bool {
+        true
     }
 }
