1dc6901034e3bfd985ec99f6e13c874fefea9ce8
billythedummy <dhy1996@live.com.sg>
2022-06-13 15:02:43 +0800

    WIP, scaffolding

--- programs/unstake/src/instructions/mod.rs
+++ programs/unstake/src/instructions/mod.rs
@@ -1,9 +1,11 @@
 mod add_liquidity;
 mod create_pool;
+mod deactivate_stake_account;
 mod remove_liquidity;
 mod unstake;
 
 pub use add_liquidity::*;
 pub use create_pool::*;
+pub use deactivate_stake_account::*;
 pub use remove_liquidity::*;
 pub use unstake::*;
