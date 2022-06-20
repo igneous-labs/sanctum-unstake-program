7aeed62d76ea986a78567f84bef3cb4422bf7176
Jesse Y. Cho <jesse@socean.fi>
2022-06-20 20:48:29 +0900

    initial impl with todos; use spl/stake-prog-add-stake-program-id branch

--- programs/unstake/src/consts.rs
+++ programs/unstake/src/consts.rs
@@ -1,1 +1,2 @@
 pub const SOL_DECIMALS: u8 = 9;
+pub const STAKE_AUTHORITY_SEED: &[u8] = b"unstake";
--- programs/unstake/src/instructions/deactivate_stake_account.rs
+++ programs/unstake/src/instructions/deactivate_stake_account.rs
@@ -1,13 +1,61 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::sysvar::SysvarId};
+use anchor_spl::stake::{self, DeactivateStake, Stake, StakeAccount};
 
+use crate::consts::STAKE_AUTHORITY_SEED;
 //use crate::errors::UnstakeError;
 
 #[derive(Accounts)]
-pub struct DeactivateStakeAccount {}
+pub struct DeactivateStakeAccount<'info> {
+    /// The stake account to be deactivated
+    #[account(
+        mut,
+        // TODO: check stake state and authority @ { WrongState, Unauthorized }
+        // TODO: should we add utility function to anchor-spl::stake?
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
 
-impl DeactivateStakeAccount {
+    // TODO
+    // - unstake ix sets it
+    // - TBD how many authorities do we need? Just single one for every stake account? Per pool (then we need to accept pool acc)?
+    ///
+    /// CHECK: this will be retyped
+    #[account(
+        seeds = [STAKE_AUTHORITY_SEED],
+        bump,
+    )]
+    pub stake_authority: UncheckedAccount<'info>,
+
+    #[account(
+        // TODO: Do we need a check here? A new Error?
+        constraint = Clock::check_id(clock.key),
+    )]
+    /// CHECK: need to check this
+    pub clock: UncheckedAccount<'info>,
+    pub stake_program: Program<'info, Stake>,
+}
+
+impl<'info> DeactivateStakeAccount<'info> {
     #[inline(always)]
-    pub fn run(_ctx: Context<Self>) -> Result<()> {
-        Ok(())
+    pub fn run(ctx: Context<Self>) -> Result<()> {
+        let stake_account = &mut ctx.accounts.stake_account;
+        let stake_authority = &ctx.accounts.stake_authority;
+        let stake_program = &ctx.accounts.stake_program;
+        let clock = &ctx.accounts.clock;
+
+        // cpi to deactivate stake
+        let deactivate_cpi_accs = DeactivateStake {
+            stake: stake_account.to_account_info(),
+            staker: stake_authority.to_account_info(),
+            clock: clock.to_account_info(),
+        };
+
+        // TODO: the actual seeds TBD
+        let seeds: &[&[u8]] = &[STAKE_AUTHORITY_SEED];
+
+        stake::deactivate_stake(CpiContext::new_with_signer(
+            stake_program.to_account_info(),
+            deactivate_cpi_accs,
+            &[seeds],
+        ))
     }
 }
