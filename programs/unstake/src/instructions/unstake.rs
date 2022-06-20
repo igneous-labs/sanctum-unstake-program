638026107f4dc1c1b43519ccfad609cebdcf948b
Jesse Y. Cho <jesse@socean.fi>
2022-06-21 01:37:44 +0900

    wip: initial impl with with todos
    use spl/stake-prog-re-export branch for StakeAuthorize

--- programs/unstake/src/instructions/unstake.rs
+++ programs/unstake/src/instructions/unstake.rs
@@ -1,8 +1,12 @@
-use anchor_lang::prelude::*;
-use anchor_spl::stake::StakeAccount;
+use anchor_lang::{prelude::*, solana_program::sysvar::SysvarId};
+use anchor_spl::stake::{self, Authorize, Stake, StakeAccount, StakeAuthorize};
+use std::collections::HashSet;
 
-use crate::anchor_len::AnchorLen;
-use crate::state::{Pool, StakeAccountRecord};
+use crate::{
+    anchor_len::AnchorLen,
+    errors::UnstakeError,
+    state::{Pool, StakeAccountRecord},
+};
 
 #[derive(Accounts)]
 pub struct Unstake<'info> {
@@ -12,6 +16,14 @@ pub struct Unstake<'info> {
 
     ///
     pub pool: Account<'info, Pool>,
+
+    ///
+    /// CHECK: this will be retyped
+    #[account(
+        seeds = [b"unstake"],  // TODO TBD temp value. Should we just use pool_sol_reserve?
+        bump,
+    )]
+    pub stake_authority: UncheckedAccount<'info>,
 
     ///
     #[account(
@@ -33,24 +45,63 @@ pub struct Unstake<'info> {
     /// CHECK: payment destination that can accept sol transfer
     pub destination: UncheckedAccount<'info>,
 
+    #[account(
+        // TODO: Do we need a check here? A new Error?
+        constraint = Clock::check_id(clock.key),
+    )]
+    /// CHECK: need to check this
+    pub clock: UncheckedAccount<'info>,
+    pub stake_program: Program<'info, Stake>,
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> Unstake<'info> {
-    const _SIMPLE_FLAT_FEE: u64 = 1_000_000_000;
-
     #[inline(always)]
     pub fn run(ctx: Context<Self>) -> Result<()> {
-        // access the given stake account data
-        let _stake_account = &mut ctx.accounts.stake_account;
+        let stake_account = &mut ctx.accounts.stake_account;
+        let stake_program = &ctx.accounts.stake_program;
+        let unstaker = &ctx.accounts.unstaker;
+        let stake_authority = &ctx.accounts.stake_authority;
+        let stake_account_record = &mut ctx.accounts.stake_account_record;
+        let clock = &ctx.accounts.clock;
+
+        let authorized = stake_account
+            .authorized()
+            .ok_or(UnstakeError::StakeAccountAuthorizedNotRetrievable)?;
+        // NOTE: check for withdrawer authority only since withdrawer can change both
+        authorized
+            .check(&HashSet::from([unstaker.key()]), StakeAuthorize::Withdrawer)
+            .map_err(|_| UnstakeError::StakeAccountNotOwned)?;
+
+        // cpi to stake::Authorize
+        // TODO: should we derive Clone?
+        let authorize_cpi_accs = Authorize {
+            stake: stake_account.to_account_info(),
+            authorized: unstaker.to_account_info(),
+            new_authorized: stake_authority.to_account_info(), // TODO: the program's stake authority
+            clock: clock.to_account_info(),
+        };
+        // authorize stake_authority as Withdrawer
+        stake::authorize(
+            CpiContext::new(stake_program.to_account_info(), authorize_cpi_accs),
+            StakeAuthorize::Withdrawer,
+            None, // custodian
+        )?;
+        // TODO: authorize stake_authority as Staker
+        //stake::authorize(
+        //    CpiContext::new(stake_program.to_account_info(), authorize_cpi_accs),
+        //    StakeAuthorize::Staker,
+        //    None, // custodian
+        //)?;
 
         // populate the stake_account_record
-        //
-        Ok(())
-    }
+        // TODO: confirm if this value need to exclude rent exampt reserve
+        //let meta = stake_account.meta();
+        //meta.rent_exampt_reserve;
+        stake_account_record.lamports_at_creation = stake_account.to_account_info().lamports();
 
-    // TODO: predicate that validates given the state StakeAccount
-    pub fn validate_stake_account_state(&self) -> bool {
-        true
+        // TODO: pay-out from lp
+
+        Ok(())
     }
 }
