ff9c96e2313d64c0c69b4a013bcafdb6ead90036
Jesse Y. Cho <jesse@socean.fi>
2022-06-21 16:38:04 +0900

    add sol transfer for unstake

--- programs/unstake/src/instructions/unstake.rs
+++ programs/unstake/src/instructions/unstake.rs
@@ -1,6 +1,7 @@
 use anchor_lang::{
     prelude::*,
     solana_program::{stake::state::StakeAuthorize, sysvar::SysvarId},
+    system_program,
 };
 use anchor_spl::stake::{self, Authorize, Stake, StakeAccount};
 use std::collections::HashSet;
@@ -13,11 +14,23 @@ use crate::{
 
 #[derive(Accounts)]
 pub struct Unstake<'info> {
-    ///
+    /// stake account owner
     #[account(mut)]
     pub unstaker: Signer<'info>,
 
-    ///
+    /// stake account to be unstaked
+    #[account(
+        mut,
+        // TODO: constraint -> must be owned by the unstaker
+        // TODO: constraint -> must not be locked (Deligated or Initialized)
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    /// Solana native wallet pubkey to receive the unstaked amount
+    /// CHECK: payment destination that can accept sol transfer
+    pub destination: SystemAccount<'info>,
+
+    /// pool account that SOL reserves belong to
     pub pool_account: Account<'info, Pool>,
 
     /// pool's SOL reserves
@@ -28,25 +41,13 @@ pub struct Unstake<'info> {
     )]
     pub pool_sol_reserves: SystemAccount<'info>,
 
-    ///
-    #[account(
-        mut,
-        // TODO: constraint -> must be owned by the unstaker
-        // TODO: constraint -> must not be locked (Deligated or Initialized)
-    )]
-    pub stake_account: Account<'info, StakeAccount>,
-
-    /// (PDA)
+    /// stake account record to be created
     #[account(
         init,
         payer = unstaker,
         space = StakeAccountRecord::LEN,
     )]
     pub stake_account_record: Account<'info, StakeAccountRecord>,
-
-    /// Solana native wallet pubkey to receive the unstaked amount
-    /// CHECK: payment destination that can accept sol transfer
-    pub destination: UncheckedAccount<'info>,
 
     #[account(
         // TODO: Do we need a check here? A new Error?
@@ -61,13 +62,15 @@ pub struct Unstake<'info> {
 impl<'info> Unstake<'info> {
     #[inline(always)]
     pub fn run(ctx: Context<Self>) -> Result<()> {
-        let stake_account = &mut ctx.accounts.stake_account;
-        let stake_program = &ctx.accounts.stake_program;
         let unstaker = &ctx.accounts.unstaker;
+        let stake_account = &mut ctx.accounts.stake_account;
+        let destination = &ctx.accounts.destination;
         let _pool_account = &ctx.accounts.pool_account;
         let pool_sol_reserves = &ctx.accounts.pool_sol_reserves;
         let stake_account_record = &mut ctx.accounts.stake_account_record;
         let clock = &ctx.accounts.clock;
+        let stake_program = &ctx.accounts.stake_program;
+        let system_program = &ctx.accounts.system_program;
 
         let authorized = stake_account
             .authorized()
@@ -109,9 +112,20 @@ impl<'info> Unstake<'info> {
         // TODO: confirm if this value need to exclude rent exampt reserve
         //let meta = stake_account.meta();
         //meta.rent_exampt_reserve;
-        stake_account_record.lamports_at_creation = stake_account.to_account_info().lamports();
+        let amount = stake_account.to_account_info().lamports();
+        stake_account_record.lamports_at_creation = amount;
 
-        // TODO: pay-out from lp
+        // pay out from the pool reserves
+        // TODO: fee collection
+        // TODO: what should happen if the balance is not enough to serve the transaction?
+        let transfer_cpi_accs = system_program::Transfer {
+            from: pool_sol_reserves.to_account_info(),
+            to: destination.to_account_info(),
+        };
+        system_program::transfer(
+            CpiContext::new(system_program.to_account_info(), transfer_cpi_accs),
+            amount,
+        )?;
 
         // TODO: update pool_account
 
