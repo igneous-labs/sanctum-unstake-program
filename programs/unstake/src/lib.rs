05ed592c37eaf141152c3a5b8deeab58097e16f9
billythedummy <dhy1996@live.com.sg>
2022-07-12 13:54:19 +0800

    update mainnet key

--- programs/unstake/src/lib.rs
+++ programs/unstake/src/lib.rs
@@ -3,9 +3,8 @@ use anchor_lang::prelude::*;
 #[cfg(feature = "local-testing")]
 declare_id!("6KBz9djJAH3gRHscq9ujMpyZ5bCK9a27o3ybDtJLXowz");
 
-// TODO: replace with real deployment key
 #[cfg(not(feature = "local-testing"))]
-declare_id!("3zSwHpEF8svwihadvnx7q2EagTyW1tvwn354gzvF5Zh4");
+declare_id!("unpXTU2Ndrc7WWNyEhQWe4udTzSibLPi25SXv2xbCHQ");
 
 pub mod anchor_len;
 pub mod consts;
