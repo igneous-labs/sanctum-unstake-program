b5fff4db784b0cc21fda229e034e00651525647e
billythedummy <dhy1996@live.com.sg>
2022-08-16 17:03:32 +0800

    refactor unstake to factor out common bits

--- programs/unstake/src/instructions/mod.rs
+++ programs/unstake/src/instructions/mod.rs
@@ -4,12 +4,12 @@ mod deactivate_stake_account;
 mod reclaim_stake_account;
 mod remove_liquidity;
 mod set_fee;
-mod unstake;
+mod unstake_instructions;
 
-pub use self::unstake::*;
 pub use add_liquidity::*;
 pub use create_pool::*;
 pub use deactivate_stake_account::*;
 pub use reclaim_stake_account::*;
 pub use remove_liquidity::*;
 pub use set_fee::*;
+pub use unstake_instructions::*;
---
+++ programs/unstake/src/instructions/unstake_instructions/mod.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+mod unstake_accounts;
+
+mod unstake;
+
+pub use self::unstake::*;
