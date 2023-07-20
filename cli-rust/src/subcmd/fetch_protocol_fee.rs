751028bc7f11bc6d2c0dce9a65959e00ff3aa39a
Johnnycus <sirjsx@gmail.com>
2023-07-20 14:38:49 +0600

    finish fetch-protocol-fee cmd

--- cli-rust/src/subcmd/fetch_protocol_fee.rs
+++ cli-rust/src/subcmd/fetch_protocol_fee.rs
@@ -1,6 +1,10 @@
+use anchor_lang::AccountDeserialize;
 use clap::Args;
 use solana_program::pubkey::Pubkey;
-use unstake::{state::PROTOCOL_FEE_SEED, ID};
+use unstake::{
+    state::{ProtocolFee, PROTOCOL_FEE_SEED},
+    ID,
+};
 
 use super::SubcmdExec;
 
@@ -10,12 +14,13 @@ pub struct FetchProtocolFeeArgs {}
 
 impl SubcmdExec for FetchProtocolFeeArgs {
     fn process_cmd(&self, args: &crate::Args) {
-        let payer = args.config.signer();
         let client = args.config.rpc_client();
 
         let protocol_fee_account = Pubkey::find_program_address(&[PROTOCOL_FEE_SEED], &ID);
 
-        let pf = client.get_account_data(&protocol_fee_account.0).unwrap();
+        let account = client.get_account_data(&protocol_fee_account.0).unwrap();
+
+        let pf = ProtocolFee::try_deserialize(&mut account.as_slice()).unwrap();
 
         println!("Protocol Fee: {:?}", pf);
     }
