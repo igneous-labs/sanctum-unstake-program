62f3309916b42bba2cef0bfed18ccecb2b634cb3
Johnnycus <sirjsx@gmail.com>
2023-07-21 15:24:15 +0600

    add init_protocol_fee

---
+++ cli-rust/src/subcmd/init_protocol_fee.rs
@@ -0,0 +1,41 @@
+use clap::Args;
+use solana_program::{pubkey::Pubkey, system_program};
+use solana_sdk::transaction::Transaction;
+use unstake::{state::PROTOCOL_FEE_SEED, ID};
+use unstake_interface::{init_protocol_fee_ix, InitProtocolFeeIxArgs, InitProtocolFeeKeys};
+
+use super::SubcmdExec;
+
+#[derive(Args, Debug)]
+#[command(long_about = "Initializes the protocol fee for the program")]
+pub struct InitProtocolFeeArgs {}
+
+impl SubcmdExec for InitProtocolFeeArgs {
+    fn process_cmd(&self, args: &crate::Args) {
+        let payer = args.config.signer();
+        let client = args.config.rpc_client();
+
+        let protocol_fee_account = Pubkey::find_program_address(&[PROTOCOL_FEE_SEED], &ID);
+
+        let ix = init_protocol_fee_ix(
+            InitProtocolFeeKeys {
+                payer: payer.pubkey(),
+                protocol_fee_account: protocol_fee_account.0,
+                system_program: system_program::id(),
+            },
+            InitProtocolFeeIxArgs {},
+        )
+        .unwrap();
+
+        let blockhash = client.get_latest_blockhash().unwrap();
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&payer.pubkey()),
+            &[payer.as_ref()],
+            blockhash,
+        );
+        let sig = client.send_and_confirm_transaction(&tx).unwrap();
+        println!("Protocol fee initialized at {}", protocol_fee_account.0);
+        println!("TX: {:?}", sig);
+    }
+}
